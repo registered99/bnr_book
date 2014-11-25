628cc3a37ffcaf2dac1e8696caf40452e65f2687
Justin Wong <registered99@gmail.com>
2014-11-25 11:06:29 -0500

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by Justin Wong on 25/11/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Homepwner
+//
+//  Created by Justin Wong on 25/11/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
